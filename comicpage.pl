a689198bdeec76e9ad0a723d0d29872305b0168e
Ben Hochstedler <hochstrb@users.sourceforge.net>
2001-05-10 20:52:44 +0000

    removed perl from emacs mode lines

--- comicpage.pl
+++ comicpage.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl -w
-#-*-mode: CPerl; tab-width: 4 -*-
+#-*- tab-width: 4 -*-
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
