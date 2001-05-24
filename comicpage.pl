12c749324e538bd4ba665cd721d8e32e04586357
Elliot Glaysher <glaysher@umich.edu>
2001-05-24 04:23:14 +0000

    See the Changelog

--- comicpage.pl
+++ comicpage.pl
@@ -100,7 +100,7 @@ main Gtk;
 
 sub about_Form {
 	my $name = $0;
-	#
+
 	# Create a Gnome::About '$ab'
 	my $ab = new Gnome::About
 	  (
@@ -111,6 +111,8 @@ sub about_Form {
 	   "Ben Hochstedler <hochstrb\@cs.rose-hulman.edu>", 
 	   "Comic Page is a program that will create a composite image of your favorite online comics. It will also do archiving of these comics if you so wish it to."
 	  );
+
+	# Set properties and show.
 	$ab->set_title("About"."Comic Page" );
 	$ab->position('mouse' );
 	$ab->set_policy(1, 1, 0 );
@@ -120,45 +122,62 @@ sub about_Form {
 
 sub comic_selected_from_list {
 	my ( $clist, $row, $column, $event, @data ) = @_;
-
+	my $calendar;
 	my $name;
+	$calendar = $forms->{'window_comic_page'}{'calendar_date_comic_selection'};
+
 	# Get the proc name of the comic.
 	$name = $clist->get_text( $row, 0 );
 	$proc = $name_lookup{$name};
-	# Just prints some information about the selected row
-	print "Okay! You want to view \"$name\" with the proc of \"$proc\".\n";
+
+	# Do a days behind lookup, get the localtime formatted @ for time - the
+	# days behind, and then set the $calendar's date. (Display_comic is called
+	# automatically.
+	my $days_behind = $date_lookup{$proc};
+	my @date = localtime((time - (86400*$days_behind)));
+	$calendar->select_month( $date[4], ($date[5] + 1900) );
+	$calendar->select_day( ($date[3] - 1) );
 
 	return;
 }
 
 sub Display_comic {
+	# Setup variables.
 	my $calendar;
 	$calendar = $forms->{'window_comic_page'}{'calendar_date_comic_selection'};
 	(my $year, my $month, my $day) = $calendar->get_date();
-	$info->set_progress(0);
-	print "$year:$month:$day\n";
 	my $day_to_download = mkgmtime(0,0,12,$day,$month,$year-1900);
 	my $comic_name = $procs{$proc};
 	my $days_behind = $date_lookup{$proc};
+
+	$info->set_progress(0);
+	print "$year:$month:$day\n" if $verbose;
+
+	# Check to see if the day's happened yet.
 	unless (Netcomics::Util::in_future($day_to_download, $days_behind)) {
 		$info->set_status("This day has not happened yet.");
 		return;
 	}
 
+	# Pass responsibility off to $factory to get the comic.
 	$info->set_progress(.10);
 	$info->set_status("Please wait while downloading $comic_name...");
 	my ($rli,$i) = $factory->get_comic($proc, $day_to_download);
+
 	$info->set_progress(.70);
 	if ($extra_verbose) {
 		print Data::Dumper->Dump([$rli],[qw(*rli)]);
 	}
+
 	if (defined($rli) && $rli->{'status'} == 1) {
+		# Display the comic since it downloaded succesfully.
 		my $filename = "$comics_dir/$rli->{'file'}[0]";
 		print "Going to display: $filename \n" if $extra_verbose;
 		$forms->{'window_comic_page'}{'pixmap1'}->load_file($filename);
 		$forms->{'window_comic_page'}{'pixmap1'}->show();
 		$info->set_status("Displayed: $filename");
 	} else {
+		# Error, as it didn't download.
 		$info->set_status(strftime("Could not download $comic_name for %x",
 								   gmtime($day_to_download)));
 	}
