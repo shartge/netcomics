609f75e53511973dd967a6e3c2a6f2d3bb570899
Sven Hartge <sven@svenhartge.de>
2002-06-09 22:46:38 +0000

    moved caption closer to related image into the rounded borders

--- Netcomics/HTML/Themes/CleanRound.pm
+++ Netcomics/HTML/Themes/CleanRound.pm
@@ -36,6 +36,13 @@ $html{'links'} = <<END_LINKS;
 </TABLE>
 END_LINKS
 
+$html{'caption'} = <<END_CAPTION;
+<td><img width=20 height=48 src="<THEME_DIR>/left.gif"></td>
+<td><center><font face="Arial,Helvetica,sans-serif" size=+1> <CAPTION_DATA> </font></center></td>
+<td><img width=20 height=48 src="<THEME_DIR>/right.gif"></td>
+END_CAPTION
+
+
 $html{'body'} = <<END_BODY;
   <tr><td>
       <center>
@@ -55,6 +62,7 @@ $html{'body'} = <<END_BODY;
 	  <td><ELEMENT></td>
 	  <td><img width=20 <HEIGHT> src="<THEME_DIR>/right.gif"></td>
 	</tr>
+        <CAPTION>
 	<tr>
 	  <td><img width=20 height=20 src="<THEME_DIR>/bot_l.gif"></td>
 	  <td><img <WIDTH> height=20 src="<THEME_DIR>/bot.gif"></td>
@@ -62,7 +70,6 @@ $html{'body'} = <<END_BODY;
 	</tr>
       </table>
       </center>
-      <CAPTION>
   </td></tr>
 END_BODY
 
