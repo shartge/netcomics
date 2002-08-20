44981b3766e1fa0293ad87c6b9c05d09e2142344
Sven Hartge <sven@svenhartge.de>
2002-08-20 23:53:45 +0000

    a height of 15 for the lower grey b order of the top comics identifiert was a bit too high, adjusting to 10, which should look better

--- Netcomics/HTML/Themes/CleanRound.pm
+++ Netcomics/HTML/Themes/CleanRound.pm
@@ -62,7 +62,7 @@ $html{'body'} = <<END_BODY;
         </tr>
         <tr>
 	  <td background="<THEME_DIR>/left.gif" width="20">&nbsp;</td>
-	  <td bgcolor="#f7f7f7" height="15">&nbsp;</td>
+	  <td bgcolor="#f7f7f7" height="10">&nbsp;</td>
 	  <td background="<THEME_DIR>/right.gif" width="20">&nbsp;</td>
         </tr>
         <tr>
